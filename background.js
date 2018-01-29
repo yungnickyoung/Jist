64c5d4b756ecb6c90ff390fcd6966fe64beb87f3
yungnickyoung <ndyoung96@gmail.com>
2018-01-28 23:51:08 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,15 @@
+var summaryText = "";
+
+chrome.browserAction.onClicked.addListener( function() {
+  calcSummary();
+} );
+
+function calcSummary() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {method: "getSelection", protocol: tabs[0].url.split(":")[0] }, function(response){});
+  });
+}
+
+var context = "selection";
+var title = "Get the Jist!";
+var id = chrome.contextMenus.create({"title": title, "contexts": [context], "onclick": calcSummary});
---
+++ content.js
@@ -0,0 +1,74 @@
+/*
+JQuery used under the MIT license.
+  * For more information, see https://jquery.org/license/
+
+This extension uses the 'Summa' implementation of the TextRank algorithm,
+available under the MIT license. Copyright 2014 Summa NLP.
+  * For more information, see http://summanlp.github.io/textrank/.
+*/
+
+var div1 = document.createElement( 'div' );
+document.body.appendChild(div1);
+var summaryText = "";
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.method == "getSelection") {
+
+    var div2 = document.createElement( 'div' );
+    div2.className = 'waitOverlay';
+    document.body.appendChild(div2);
+
+    var xhttp = new XMLHttpRequest();
+    var url = "";
+    if (request.protocol == "https") {
+      url = "https://yungnickyoung.pythonanywhere.com";
+    }
+    else {
+      url = "http://yungnickyoung.pythonanywhere.com";
+    }
+    var paramsObj = {"selText" : window.getSelection().toString()};
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        summaryText = this.responseText;
+        document.body.removeChild(div2);
+        testfunction(summaryText);
+      }
+    };
+
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(paramsObj));
+  }
+});
+
+function testfunction(sumtxt) {
+  div1.id = 'jistDialog';
+  div1.title = 'Jist! Summary';
+  if (sumtxt == "") {sumtxt = "Wtf";}
+  div1.innerHTML = sumtxt;
+  var winWidth = $(window).width();
+  var winHeight = $(window).height();
+  $( "#jistDialog" ).dialog({
+                              autoOpen: false,
+                              modal: true,
+                              width: (.7 * winWidth),
+                              height: (.6 * winHeight),
+                              minWidth: (.5 * winWidth),
+                              minHeight: (.5 * winHeight),
+                              maxWidth: (.9 * winWidth),
+                              maxHeight: (.9 * winHeight)
+                            });
+  $( "#jistDialog" ).dialog({
+    buttons: [
+      {
+        //text: "OK",
+        icon: "ui-icon-check",
+        click: function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    ]
+  });
+  $( "#jistDialog" ).dialog({ show: {effect: "fade", duration: 400} });
+  $( "#jistDialog" ).dialog( "open" );
+}
