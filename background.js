33fb72df385b480879cea4fa3fa138acc81d52a7
Nick Young <nicholas_young@ultimatesoftware.com>
2018-05-30 11:38:21 -0400

    Added comments, documentation. Slight refactoring to code. Corrected name in license.

--- background.js
+++ background.js
@@ -1,15 +1,25 @@
-var summaryText = "";
+var summaryText = ""; // initialize summary text
 
-chrome.browserAction.onClicked.addListener( function() {
-  calcSummary();
-} );
-
+// called when user clicks context menu item or browser extension icon
 function calcSummary() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {method: "getSelection", protocol: tabs[0].url.split(":")[0] }, function(response){});
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) { // find currently open/active tab
+    chrome.tabs.sendMessage(tabs[0].id,
+      {
+        method: "getSelection",
+        protocol: tabs[0].url.split(":")[0] // determine if HTTPS
+      },
+      function (response) { });
   });
 }
 
+// content for context menu item
 var context = "selection";
 var title = "Get the Jist!";
-var id = chrome.contextMenus.create({"title": title, "contexts": [context], "onclick": calcSummary});
+
+// create context menu item which summarizes highlighted text when clicked
+var id = chrome.contextMenus.create({ "title": title, "contexts": [context], "onclick": calcSummary });
+
+// give browser extension icon same functionality as context menu item
+chrome.browserAction.onClicked.addListener(function () {
+  calcSummary();
+});
--- content.js
+++ content.js
@@ -7,32 +7,33 @@ available under the MIT license. Copyright 2014 Summa NLP.
   * For more information, see http://summanlp.github.io/textrank/.
 */
 
-var div1 = document.createElement( 'div' );
-document.body.appendChild(div1);
-var summaryText = "";
+var divDialog = document.createElement('div');
+document.body.appendChild(divDialog);
+var success;
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.method == "getSelection") {
 
-    var div2 = document.createElement( 'div' );
-    div2.className = 'waitOverlay';
-    document.body.appendChild(div2);
+    var summaryText = "";
+    var divWaitOverlay = document.createElement('div');
+    divWaitOverlay.className = 'waitOverlay';
+    document.body.appendChild(divWaitOverlay);
 
     var xhttp = new XMLHttpRequest();
-    var url = "";
-    if (request.protocol == "https") {
-      url = "https://yungnickyoung.pythonanywhere.com";
-    }
-    else {
-      url = "http://yungnickyoung.pythonanywhere.com";
-    }
-    var paramsObj = {"selText" : window.getSelection().toString()};
-    xhttp.onreadystatechange = function() {
+    var url = (request.protocol == "https")
+      ? "https://yungnickyoung.pythonanywhere.com"
+      : "http://yungnickyoung.pythonanywhere.com";
+
+    var paramsObj = {
+      "selText": window.getSelection().toString()
+    };
+
+    xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         summaryText = this.responseText;
-        document.body.removeChild(div2);
-        _update(summaryText);
       }
+      document.body.removeChild(divWaitOverlay);
+      updateDivDialog(summaryText);
     };
 
     xhttp.open("POST", url, true);
@@ -41,34 +42,44 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   }
 });
 
-function _update(sumtxt) {
-  div1.id = 'jistDialog';
-  div1.title = 'Jist! Summary';
-  if (sumtxt == "") {sumtxt = "An error has occured.";}
-  div1.innerHTML = sumtxt;
+/* Convert the divDialog from an empty div to a pop-up dialog box
+ * containing the summarized text retrieved from the server
+ * 
+ * @param summaryText: summarized text returned from the server
+ */
+function updateDivDialog(summaryText) {
+  divDialog.id = 'jistDialog';
+  divDialog.title = 'Jist! Summary';
+
+  if (summaryText == "") {
+    summaryText = "An error has occured."; // server shouldn't return empty text
+  }
+
+  divDialog.innerHTML = summaryText;
   var winWidth = $(window).width();
   var winHeight = $(window).height();
-  $( "#jistDialog" ).dialog({
-                              autoOpen: false,
-                              modal: true,
-                              width: (.7 * winWidth),
-                              height: (.6 * winHeight),
-                              minWidth: (.5 * winWidth),
-                              minHeight: (.5 * winHeight),
-                              maxWidth: (.9 * winWidth),
-                              maxHeight: (.9 * winHeight)
-                            });
-  $( "#jistDialog" ).dialog({
+  $("#jistDialog").dialog({
+    autoOpen: false,
+    modal: true,
+    width: (.7 * winWidth),
+    height: (.6 * winHeight),
+    minWidth: (.5 * winWidth),
+    minHeight: (.5 * winHeight),
+    maxWidth: (.9 * winWidth),
+    maxHeight: (.9 * winHeight)
+  });
+
+  $("#jistDialog").dialog({
     buttons: [
       {
         //text: "OK",
         icon: "ui-icon-check",
-        click: function() {
-          $( this ).dialog( "close" );
+        click: function () {
+          $(this).dialog("close");
         }
       }
     ]
   });
-  $( "#jistDialog" ).dialog({ show: {effect: "fade", duration: 400} });
-  $( "#jistDialog" ).dialog( "open" );
+  $("#jistDialog").dialog({ show: { effect: "fade", duration: 400 } });
+  $("#jistDialog").dialog("open");
 }
